7f5815fba2ab4cbb351e1fd8df4846ca93821768
IT-1-F-30 <135986508+IT-1-F-30@users.noreply.github.com>
2024-11-19 22:53:29 +0900

    change(1)

--- assets/favorite.js
+++ assets/favorite.js
@@ -12,23 +12,22 @@ async function articles_json(value) {
     const key = Object.keys(articlesData).find(key => articlesData[key] === parseInt(value));
     return key;
 }
+// fetch('https://script.google.com/macros/s/AKfycbyDdSAGPG3I1d0TBkyfi0ZVyAG-fPswQI_7k9rM2rz2GJYZUM_lDbcNoAuQuA4ObybR/exec?mode=list')
+//   .then(response => response.json())
+//   .then(async data => {
+//         let textbox_element = document.getElementById('textbox');
+//         for (i of data["value"]) {
+//             let new_element = document.createElement('p');
+//             new_element.textContent = i[1];
 
-fetch('https://script.google.com/macros/s/AKfycbyDdSAGPG3I1d0TBkyfi0ZVyAG-fPswQI_7k9rM2rz2GJYZUM_lDbcNoAuQuA4ObybR/exec?mode=list')
-  .then(response => response.json())
-  .then(async data => {
-        let textbox_element = document.getElementById('textbox');
-        for (i of data["value"]) {
-            let new_element = document.createElement('p');
-            new_element.textContent = i[1];
-
-            let combined_element = document.createDocumentFragment();
-            combined_element.appendChild(document.createTextNode(await articles_json(i[0])));
+//             let combined_element = document.createDocumentFragment();
+//             combined_element.appendChild(document.createTextNode(await articles_json(i[0])));
             
-            combined_element.appendChild(new_element);
-            textbox_element.appendChild(combined_element);
-        }
-  })
-  .catch(error => console.error('Error:', error));
+//             combined_element.appendChild(new_element);
+//             textbox_element.appendChild(combined_element);
+//         }
+//   })
+//   .catch(error => console.error('Error:', error));
 
 function addURL() {
     const url = "https://script.google.com/macros/s/AKfycbyDdSAGPG3I1d0TBkyfi0ZVyAG-fPswQI_7k9rM2rz2GJYZUM_lDbcNoAuQuA4ObybR/exec?mode=write&key=10000&value=1";
